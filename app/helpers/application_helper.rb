d4345602c34135661831db10a532c7968e410b58
Frikker <stolpnikk.97@gmail.com>
2019-07-13 13:18:52 +0300

    first commit since pause

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def full_title(page_name = '')
+    base_name = 'Ruby on Rails Tutorial Application'
+    page_name.empty? ? base_name : "#{page_name} | #{base_name}"
+  end
 end
